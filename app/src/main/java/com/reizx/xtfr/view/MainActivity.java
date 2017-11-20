84834c8edfab33e70635eae51aaea2b23a13de04
KigKrazy <KigKrazy@163.com>
2017-11-20 11:11:29 +0800

    增加OPPO的测试代码

--- app/src/main/java/com/reizx/xtfr/view/MainActivity.java
+++ app/src/main/java/com/reizx/xtfr/view/MainActivity.java
@@ -38,7 +38,11 @@ public class MainActivity extends AppCompatActivity {
     @OnClick(R.id.btn_get_rom_name)
     public void getRomName(){
         /**
-         * Oppo中的一个获取Display设备rom版本号的获取方法，此种方法并非直接“Build.DISPLAY”,参数没有替换到。
+         * Oppo：
+         * 分析位置：
+         * com.nearme.common.util.DeviceUtil#getRomName
+         *
+         * 获取Display设备rom版本号的获取方法，此种方法并非直接“Build.DISPLAY”,参数没有替换到。
          * 要求增加android.os.SystemPropertiesd的HOOK，并且将Build的参数Hook进去。
          */
         String romName = Reflect.on("android.os.SystemProperties").call("get","ro.build.display.id", "").get();
